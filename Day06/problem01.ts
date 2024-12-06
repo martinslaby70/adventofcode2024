cfc58f7688c31933da2ba490c5e07a6870decc0a
Martin Slabý <martin.slaby29@seznam.cz>
2024-12-06 07:48:18 +0100

    Day06: part1&part2

---
+++ Day06/problem01.ts
@@ -0,0 +1,65 @@
+import { input } from "./input";
+
+const maze = input.split("\n").map((rows) => rows.split(""));
+
+let result = 1;
+let position: [number, number] = [0, 0];
+let direction = "";
+let defaultDirection = "";
+
+maze.find((row, rowIndex) =>
+  row.find((char, charIndex) => {
+    if (char === "." || char === "#") return false;
+
+    position = [rowIndex, charIndex];
+    direction = char;
+    defaultDirection = char;
+  })
+);
+
+while (true) {
+  const nextRow = "v^".includes(direction)
+    ? position[0] + (direction === "^" ? -1 : 1)
+    : position[0];
+
+  const nextChar = "><".includes(direction)
+    ? position[1] + (direction === "<" ? -1 : 1)
+    : position[1];
+
+  const nextStep = maze[nextRow]?.[nextChar];
+
+  if (nextStep === undefined) break;
+
+  if (nextStep === ".") {
+    result++;
+    position = [nextRow, nextChar];
+    maze[nextRow][nextChar] = "X";
+  }
+
+  console.log(nextRow, nextChar, nextStep);
+
+  if (nextStep === "X" || nextStep === defaultDirection) {
+    position = [nextRow, nextChar];
+  }
+
+  if (nextStep === "#") {
+    switch (direction) {
+      case ">":
+        direction = "v";
+        break;
+      case "v":
+        direction = "<";
+        break;
+      case "<":
+        direction = "^";
+        break;
+      case "^":
+        direction = ">";
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+console.log(result);
---
+++ Day06/problem02.ts
@@ -0,0 +1,84 @@
+import { input } from "./input";
+
+const maze = input.split("\n");
+const rowsLength = maze.length;
+const colLength = maze[0].length;
+
+const directions = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+
+let startRow = 0;
+let startCol = 0;
+let startDir = 0;
+
+for (let row = 0; row < rowsLength; row++) {
+  for (let column = 0; column < colLength; column++) {
+    if ("^>v<".includes(maze[row][column])) {
+      startRow = row;
+      startCol = column;
+      startDir = "^>v<".indexOf(maze[row][column]);
+      break;
+    }
+  }
+}
+
+const simulateWithObstacle = (obstacleRow: number, obstacleCol: number) => {
+  let guardRow = startRow;
+  let guardCol = startCol;
+  let guardDir = startDir;
+
+  const checked = new Set();
+
+  checked.add(`${guardRow},${guardCol},${guardDir}`);
+
+  while (true) {
+    const newDirection = directions[guardDir];
+    const nextRow = guardRow + newDirection[0];
+    const nextCol = guardCol + newDirection[1];
+
+    if (
+      nextRow < 0 ||
+      nextRow >= rowsLength ||
+      nextCol < 0 ||
+      nextCol >= colLength
+    ) {
+      return false;
+    }
+
+    const nextStep =
+      nextRow === obstacleRow && nextCol === obstacleCol
+        ? "#"
+        : maze[nextRow][nextCol];
+
+    if (nextStep === "#") {
+      guardDir = (guardDir + 1) % 4;
+    } else {
+      guardRow = nextRow;
+      guardCol = nextCol;
+    }
+
+    const params = `${guardRow},${guardCol},${guardDir}`;
+    if (checked.has(params)) {
+      return true;
+    } else {
+      checked.add(params);
+    }
+  }
+};
+
+let result = 0;
+
+for (let row = 0; row < rowsLength; row++) {
+  for (let column = 0; column < colLength; column++) {
+    if (maze[row][column] === "#" || (row === startRow && column === startCol))
+      continue;
+
+    if (simulateWithObstacle(row, column)) result++;
+  }
+}
+
+console.log(result);
